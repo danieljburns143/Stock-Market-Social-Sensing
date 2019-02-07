a6cb1f31c44063e196bf564831fd39d65df3e7c1
Daniel Burns <danieljburns143@gmail.com>
2019-02-07 18:30:02 +0000

    Docs: stockScraper

---
+++ src/stockScraper.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import json
+import requests
+
+class StockScraper():
+
+	def __init__(self):
+		self.apiKey = '7IG1A5W6UY6CAZQI'
+		self.queryURL = 'https://www.alphavantage.co/query'
+	
+	def getTimeSeriesDaily(self, symbol, outputsize='compact', datatype='json'):
+		payload = {'function': 'TIME_SERIES_DAILY', 'symbol': symbol, \
+			'outputsize': outputsize, 'datatype': datatype, 'apikey': self.apiKey}
+		return requests.get(self.queryURL, params=payload).json()
+	
+	def getTimeSeriesWeekly(self, symbol, datatype='json'):
+		payload = {'function': 'TIME_SERIES_WEEKLY', 'symbol': symbol, \
+			'datatype': datatype, 'apikey': self.apiKey}
+		return requests.get(self.queryURL, params=payload).json()
+	
+	def getTimeSeriesMonthly(self, symbol, datatype='json'):
+		payload = {'function': 'TIME_SERIES_MONTHLY', 'symbol': symbol, \
+			'datatype': datatype, 'apikey': self.apiKey}
+		return requests.get(self.queryURL, params=payload).json()
+
+stockScraper = StockScraper()
+print(stockScraper.getTimeSeriesMonthly('SQ')['Meta Data'])

a50e5a2f4374791041e41684786607a288bc72b1
Daniel Burns <danieljburns143@gmail.com>
2019-02-20 21:54:55 +0000

    Feat: Sorting tweets

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-from GetOldTweets_python import *
-from src.twitter_scripts.twitterScraper import *
-import src.text_scripts.textAnalysis 
+
 import json
 
-influencers = {}
+from GetOldTweets_python import *
+
+from src.twitter_scripts.twitterScraper import *
+import src.text_scripts.textAnalysis 
 
 def main():
 	with open('./data/Tesla/Tesla_stock.json', 'r') as f:
@@ -15,14 +16,15 @@ def main():
 
 
 def getTopTweets(): 
-	twitterScraper = TwitterScraper(since="2018-11-30", until="2018-12-13", query="tesla")
+	twitterScraper = TwitterScraper(since="2018-12-11", until="2018-12-12", query="tesla")
 	ts = twitterScraper.getTweets()
-	toptweets = sorted(ts, key = lambda x: x.favorites)[:10]
-	for tweet in top_tweets:
-		if tweet.id not in influencers.keys():
+	topTweets = sorted(ts, key = lambda x: x.favorites)[-10:]
+	for tweet in topTweets:
+		if tweet.id in influencers.keys():
 			influencers[tweet.id].append(tweet)
 		else: 
 			influencers[tweet.id] = [tweet]
 
-if __name__=="__main__":
+if __name__ == '__main__':
+	influencers = {}
 	main()

d8b9fe3cfff887d89c58210e9bb63ebfa8aea0fa
Daniel Burns <dburns3@nd.edu>
2019-04-24 15:59:28 +0000

    Tests: vaderSentiment

--- src/text_scripts/test_unit_textAnalysis.py
+++ src/text_scripts/test_unit_textAnalysis.py
@@ -4,5 +4,8 @@ from src.text_scripts.textAnalysis import *
 
 class TestTextAnalysis():
 
-	def test_getSentiment(self):
-		assert(getSentiment('This is a good test of textblob') != None)
+	def test_getSentimentBlob(self):
+		assert(getSentimentBlob('This is a good test of textblob') != None)
+	
+	def test_getSentimentVader(self):
+		assert(getSentimentVader('This is a good test of vaderSentiment') != None)

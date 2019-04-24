c1dba4d303953b8a2d77462a6036729071662ba5
pbaumann <pbaumann@nd.edu>
2019-04-24 17:31:32 +0000

    made main.py more dynamic for multiple different stocks, should allow for greater collection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import json
 import re
 from src.stock_scripts.stockScraper import StockScraper
@@ -7,22 +8,31 @@ from src.text_scripts import textAnalysis
 from src.twitter_scripts.twitterScraper import TwitterScraper
 from src.heatmap_scripts.heatMapper import heatMapper
 
-
 def getTweets(): 
     tweetScraper = TwitterScraper(since="2019-02-28", until="2019-03-07", query="canada goose")
     tweets = tweetScraper.getTweets()
+    tweetfile = open(os.path.join(os.path.dirname(__file__),
+        "data/Canada_Goose/Canada_Goose_tweets.txt"), "w")
     for tweet in tweets:
-        print(tweet.text)
-        print(textAnalysis.getSentimentBlob(tweet.text).polarity)
-        print(textAnalysis.getSentimentVader(tweet.text))
+        tweetfile.write(tweet.text + "\n")
+
+def getSentiment():
+    sentfile = open(os.path.join(os.path.dirname(__file__),
+        "data/Canada_Goose/Canada_Goose_tweets_polarity.txt"), "w")
+    with open(os.path.join(os.path.dirname(__file__),
+        "data/Canada_Goose/Canada_Goose_tweets.txt"),"r") as f:
+        for line in f:
+            sentfile.write(str(textAnalysis.getSentimentVader(line))+"\n")
+
 
 def getStocks():
-	stockScraper = StockScraper()
-	stockPrices = stockScraper.getTimeSeriesDaily('GOOS')
-	print(stockPrices.text)
+    stockScraper = StockScraper()
+    stockPrices = stockScraper.getTimeSeriesDaily('GOOS')
+    print(stockPrices.text)
 
 def main():
-	getTweets()
+    getTweets()
+    getSentiment()
 
 if __name__ == '__main__':
-		main()
+    main()

d0427b1653187c746faf53e7f1f56fd90a8d0479
pbaumann <pbaumann@nd.edu>
2019-04-28 23:54:56 +0000

    cluster has polarity sum bug

--- cluster.py
+++ cluster.py
@@ -4,61 +4,68 @@
 
 import Levenshtein
 import sys
+import os
+import json
 from src.text_scripts import textAnalysis
 
 class Cluster:
     def __init__(self, tweet):
         self.center = tweet
-        self.nearby = {}
-        self.avg_polarity = 0
+        self.nearby = []
+        self.total_polarity = 0.0
 
     def add_tweet(self, tweet, polarity, distance):
-        self.nearby[tweet] = [polarity, distance]
-        self.avg_polarity = (len(nearby.keys()-1)*self.avg_polarity + polarity) / (len(nearby.keys()))
+        self.nearby.append([tweet, polarity, distance])
+        print (self.total_polarity)
+        self.total_polarity += polarity
+        print (self.total_polarity)
+        print ("--")
 
-    def readPolarity(path="data/Canada_Goose/Canada_Goose_tweets_polarity.txt"):
-        polarity = []
-        with open(os.path.join(os.path.dirname(__file__),path),"r") as f:
-            for line in f:
-                polarity.append(line.strip())
+def readPolarity(path="data/Canada_Goose/Canada_Goose_tweets_polarity.txt"):
+    polarity = []
+    with open(os.path.join(os.path.dirname(__file__),path),"r") as f:
+        for line in f:
+            data = json.loads(line.strip().replace('\'',"\""))
+            polarity.append(data['compound'])
+    return polarity
 
-    def readTweets(path="data/Canada_Goose/Canada_Goose_tweets.txt"):
-        tweets = []
-        with open(os.path.join(os.path.dirname(__file__),path),"r") as f:
-            for line in f:
-                tweets.append(line.strip())
+def readTweets(path="data/Canada_Goose/Canada_Goose_tweets.txt"):
+    tweets = []
+    with open(os.path.join(os.path.dirname(__file__),path),"r") as f:
+        for line in f:
+            tweets.append(line.strip())
+    return tweets
 
-    def compareTweets(t1,t2):
-        return Levenshtein.ratio(t1,t2)
+def compareTweets(t1,t2):
+    return Levenshtein.ratio(t1,t2)
 
-    def measureTweets(tweets):
-        similarity_matrix = [[0]*len(tweets)]*len(tweets)
-        for x,tweet1 in enumerate(tweets):
-            for y,tweet2 in enumerate(tweets):
-                similarity_matrix[x][y]= compareTweets(tweet1,tweet2)
-        return similarity_matrix
+def measureTweets(tweets):
+    similarity_matrix = [[0]*len(tweets)]*len(tweets)
+    for x,tweet1 in enumerate(tweets):
+        for y,tweet2 in enumerate(tweets):
+            similarity_matrix[x][y]= compareTweets(tweet1,tweet2)
+    return similarity_matrix
 
-    def clusterTweets(similarity_matrix, bound, tweets, polarity):
-        clustered = []
-        clusters = []
-        for i,row in enumerate(similarity_matrix):
-            for j, similarity in enumerate(row):
-                if similarity > bound and i not in clustered:
-                    center = Cluster(tweets[i])
-                    center.add_tweet(tweets[j], polarity[j], similarity)
-                    clustered.append(i)
-                    clusters.append(center)
-                else if similarity > bound:
-                    center.add_tweet(tweets[j],polarity[j], similarity)
-        return clusters
+def clusterTweets(similarity_matrix, bound, tweets, polarity):
+    clusters = []
+    print "!!!!!!!"
+    print polarity
+    for i,row in enumerate(similarity_matrix):
+        center = Cluster(tweets[i])
+        for j, similarity in enumerate(row):
+            if similarity > bound:
+                center.add_tweet(tweets[j], polarity[j], similarity)
+        clusters.append(center)
+    return clusters
 
 def main():
     tweets = readTweets()
     polarities = readPolarity()
     matrix = measureTweets(tweets)
     clustered_tweets = clusterTweets(matrix, 0.75, tweets, polarities)
-    for cluster in sorted(clustered_tweets, key=lambda x: len(x.nearby)):
-        print ("Text: " + cluster.center + "\nPolarity: " cluster.avg_polarity + "\nInfluence: " + len(cluster.nearby))
-        
+    #for cluster in sorted(clustered_tweets, key=lambda x: len(x.nearby)):
+    #    print ("Text: " + str(cluster.center) + "\nPolarity:"
+    #            + str(cluster.total_polarity) + "\nInfluence:" + str(len(cluster.nearby)))
+    #    print ("\n") 
 if __name__=='__main__':
     main()

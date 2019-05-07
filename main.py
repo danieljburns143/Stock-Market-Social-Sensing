5a5199e91976264f6fdf499e51d20aae3ecedf08
pbaumann <pbaumann@nd.edu>
2019-05-07 15:11:59 +0000

    Updated to CG

--- main.py
+++ main.py
@@ -9,26 +9,26 @@ from src.twitter_scripts.twitterScraper import TwitterScraper
 from src.heatmap_scripts.heatMapper import heatMapper
 
 def getTweets(): 
-    tweetScraper = TwitterScraper(since="2019-04-03", until="2019-04-10",
-            query="coca cola")
+    tweetScraper = TwitterScraper(since="2019-02-28", until="2019-03-07",
+            query="canada goose")
     tweets = tweetScraper.getTweets()
     tweetfile = open(os.path.join(os.path.dirname(__file__),
-        "data/Coca_Cola/Coca_Cola_tweets.txt"), "w")
+        "data/Canada_Goose/Canada_Goose_tweets.txt"), "w")
     for tweet in tweets:
         tweetfile.write(tweet.text + "\n")
 
 def getSentiment():
     sentfile = open(os.path.join(os.path.dirname(__file__),
-        "data/Coca_Cola/Coca_Cola_tweets_polarity.txt"), "w")
+        "data/Canada_Goose/Canada_Goose_tweets_polarity.txt"), "w")
     with open(os.path.join(os.path.dirname(__file__),
-        "data/Coca_Cola/Coca_Cola_tweets.txt"),"r") as f:
+        "data/Canada_Goose/Canada_Goose_tweets.txt"),"r") as f:
         for line in f:
             sentfile.write(str(textAnalysis.getSentimentVader(line))+"\n")
 
 
 def getStocks():
     stockfile = open(os.path.join(os.path.dirname(__file__),
-    "data/Coca_Cola/Coca_Cola_stocks.json"), "w")
+    "data/Canada_Goose/Canada_Goose_stocks.json"), "w")
     stockScraper = StockScraper()
     stockPrices = stockScraper.getTimeSeriesDaily('KO')
     for line in stockPrices.text:
